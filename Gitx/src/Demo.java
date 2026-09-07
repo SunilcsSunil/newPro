4fa0fd01cfe4cb82a58fbb160f8096d166ce31b6
suni <sunilcs703@gmail.com>
2026-09-07 20:33:52 +0530

    Code change from master file

--- Gitx/src/Demo.java
+++ Gitx/src/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 		
 		System.out.println("GitX");
 		System.out.println("Code change from downloaded file");
+		System.out.println("now the change is from master file");
 		
 	}
 }
